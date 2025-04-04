ac9a6c08d03ad81f79d4bc858c2acebce655c8c0
sbergeroncp <93004917+sbergeroncp@users.noreply.github.com>
2025-04-04 16:32:41 -0400

    1.0.6

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.46/---simulator","cdnUrl":"https://cdn.makecode.com","version":"1.0.5","target":"arcade","targetVersion":"2.0.46","repo":"sbergeroncp/assets"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.46/---simulator","cdnUrl":"https://cdn.makecode.com","version":"1.0.6","target":"arcade","targetVersion":"2.0.46","repo":"sbergeroncp/assets"}
 // total=1670932 new=29.74% cached=66.03% other=4.23%
 (function (ectx) {
 'use strict';
@@ -428,7 +428,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P89890(s) {
+function _main___P89901(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -848,13 +848,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P89890.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P89890.continuations = [  ]
+_main___P89901.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P89901.continuations = [  ]
 
-function _main___P89890_mk(s) {
+function _main___P89901_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P89890, depth: s.depth + 1,
+        parent: s, fn: _main___P89901, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42434,5 +42434,5 @@ const sprites_BaseSpriteSayRenderer__C74254_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P89890
+return _main___P89901
 })
